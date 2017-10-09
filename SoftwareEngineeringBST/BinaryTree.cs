053f4e0eade1c360f79d879019f2a7f2a933e94c
Brian Whelan <whelanb7@tcd.ie>
2017-10-09 17:39:32 +0100

    Initial Commit - configure VS project and README

---
+++ SoftwareEngineeringBST/BinaryTree.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareEngineeringBST
+{
+    public class BinaryTree<T> where T : IComparable
+    {
+
+    }
+}
