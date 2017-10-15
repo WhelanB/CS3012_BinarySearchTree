0253031c2691e67a4d7fddcb8505fa9b06c69c7b
Brian Whelan <whelanb7@tcd.ie>
2017-10-15 15:10:02 +0100

    Created BinaryTree class and Node private class

--- SoftwareEngineeringBST/BinaryTree.cs
+++ SoftwareEngineeringBST/BinaryTree.cs
@@ -6,8 +6,36 @@ using System.Threading.Tasks;
 
 namespace SoftwareEngineeringBST
 {
-    public class BinaryTree<T> where T : IComparable
+    public class BinaryTree<Tkey, Tvalue> where Tkey : IComparable
     {
+        private Node root;
 
+        public BinaryTree()
+        {
+            root = null;
+        }
+
+        private class Node
+        {
+            private Tkey key;           // sorted by key
+            private Tvalue value;         // associated data
+            private Node left, right;  // left and right subtrees
+
+            public Node(Tkey key, Tvalue val)
+            {
+                this.key = key;
+                value = val;
+            }
+
+            public Tvalue GetData()
+            {
+                return value;
+            }
+
+            public override string ToString()
+            {
+                return String.Format("Key: %s, Value: %s", key.ToString(), value.ToString());
+            }
+        }
     }
 }
