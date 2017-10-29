5bfd2018e0381a5ab9ea6e3082decd7505d9a624
Brian Whelan <whelanb7@tcd.ie>
2017-10-29 16:43:28 +0000

    Implemented AddEdge in DAG with Cycle Check

--- SoftwareEngineeringBST/DirectedAcyclicGraph.cs
+++ SoftwareEngineeringBST/DirectedAcyclicGraph.cs
@@ -10,14 +10,49 @@ namespace SoftwareEngineeringBST
     {
 
         private HashSet<int>[] adj;
+        private int[] marked;
         public DirectedAcyclicGraph(int V)
         {
             adj = new HashSet<int>[V];
-         
+            marked = new int[V];
             for (int v = 0; v < V; v++)
             {
                 adj[v] = new HashSet<int>();
             }
+        }
+
+        public bool AddEdge(int v, int w)
+        {
+            if (v < 0 || v > adj.Length || w < 0 || w > adj.Length)
+                return false;
+            try
+            {
+                adj[v].Add(w);
+                for (int i = 0; i < adj.Count(); i++)
+                    marked[i] = 0;
+                for (int n = 0; n < adj.Count(); n++)
+                    if (marked[n] == 0)
+                        DfsVisit(n);
+            }
+            catch (Exception e)
+            {
+                adj[v].Remove(w);
+                return false;
+            }
+            return true;
+
+        }
+
+        private void DfsVisit(int n)
+        {
+            if (marked[n] == 2)
+                return;
+            if (marked[n] == 1)
+                throw new Exception("This graph contains a cycle, and as a result is not a directed acyclic graph");
+            marked[n] = 1;
+            foreach (int i in adj[n])
+                DfsVisit(i);
+            marked[n] = 2;
         }
 
         public override String ToString()
