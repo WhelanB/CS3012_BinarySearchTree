e4874a1b05dadae5387e646512a2d01f0eb1b984
Brian Whelan <whelanb7@tcd.ie>
2017-10-29 16:14:56 +0000

    Added base DirectedAcyclicGraph.cs

---
+++ SoftwareEngineeringBST/DirectedAcyclicGraph.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareEngineeringBST
+{
+    public class DirectedAcyclicGraph
+    {
+
+        private HashSet<int>[] adj;
+        public DirectedAcyclicGraph(int V)
+        {
+            adj = new HashSet<int>[V];
+         
+            for (int v = 0; v < V; v++)
+            {
+                adj[v] = new HashSet<int>();
+            }
+        }
+    }
+}
