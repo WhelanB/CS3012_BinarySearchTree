e7fd1274ef49bae9faa7e044bd1f0034eefa48be
Brian Whelan <whelanb7@tcd.ie>
2017-10-29 16:20:20 +0000

    Added ToString method for DirectedAcyclicGraph.cs

--- SoftwareEngineeringBST/DirectedAcyclicGraph.cs
+++ SoftwareEngineeringBST/DirectedAcyclicGraph.cs
@@ -19,5 +19,18 @@ namespace SoftwareEngineeringBST
                 adj[v] = new HashSet<int>();
             }
         }
+
+        public override String ToString()
+        {
+            String result = "";
+            for (int v = 0; v < adj.Length; v++)
+            {
+                if (adj[v].Count != 0)
+                {
+                    result += v + ": " + string.Join("|", adj[v]) + "\n";
+                }
+            }
+            return result;
+        }
     }
 }
